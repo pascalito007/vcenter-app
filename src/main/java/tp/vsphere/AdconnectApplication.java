902e990c58ff697bc9043545c37c06ba01d261b3
MEWENEMESSE Jean-Pascal <pasciano007@gmail.com>
2020-03-08 20:38:46 +0100

    update

--- src/main/java/tp/vsphere/AdconnectApplication.java
+++ src/main/java/tp/vsphere/AdconnectApplication.java
@@ -1,14 +1,44 @@
 package tp.vsphere;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 @SpringBootApplication
-public class AdconnectApplication {
+public class AdconnectApplication implements CommandLineRunner {
 
 
     public static void main(String[] args) {
         SpringApplication.run(AdconnectApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        //String command = "powershell.exe  your command";
+        //Getting the version
+        String command = "powershell.exe  $PSVersionTable.PSVersion";
+        // Executing the command
+        Process powerShellProcess = Runtime.getRuntime().exec(command);
+        // Getting the results
+        powerShellProcess.getOutputStream().close();
+        String line;
+        System.out.println("Standard Output:");
+        BufferedReader stdout = new BufferedReader(new InputStreamReader(
+                powerShellProcess.getInputStream()));
+        while ((line = stdout.readLine()) != null) {
+            System.out.println(line);
+        }
+        stdout.close();
+        System.out.println("Standard Error:");
+        BufferedReader stderr = new BufferedReader(new InputStreamReader(
+                powerShellProcess.getErrorStream()));
+        while ((line = stderr.readLine()) != null) {
+            System.out.println(line);
+        }
+        stderr.close();
+        System.out.println("Done");
+    }
 }
